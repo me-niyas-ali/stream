eb811a7c21574917bb45dcf11536d02a2b7303e5
Niyas Ali <136828928+me-niyas-ali@users.noreply.github.com>
2025-05-23 17:32:54 +0400

    update server.js

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 // server.js
 const express = require('express');
 const http = require('http');
-const {
- Server
-} = require('socket.io');
+const { Server } = require('socket.io');
 const path = require('path');
 
 const app = express();
@@ -17,65 +15,44 @@ app.use(express.static(path.join(__dirname, 'public')));
 const rooms = {};
 
 io.on('connection', socket => {
- socket.on('host-create', roomId => {
-  socket.join(roomId);
-  socket.isHost = true;
-  rooms[roomId] = rooms[roomId] || {
-   count: 0, host: socket.id
-  };
-  rooms[roomId].count = 1;
-  rooms[roomId].host = socket.id;
-  io.to(roomId).emit('update-count', rooms[roomId].count);
-
-  // Start periodic sync
-  const interval = setInterval(() => {
-   const currentTime = socket.videoCurrentTime || 0;
-   io.to(roomId).emit('host-time', currentTime);
-  }, 2000);
-
-  socket.syncInterval = interval;
- });
-
- socket.on('join-room', roomId => {
-  if (!rooms[roomId]) {
-   socket.emit('error', 'Room does not exist');
-   return;
-  }
-  socket.join(roomId);
-  rooms[roomId].count++;
-  io.to(roomId).emit('update-count', rooms[roomId].count);
-  // Ask host for current time to sync new joiner
-  socket.to(roomId).emit('sync-request', socket.id);
- });
-
- socket.on('sync-time',
-  ({
-   targetId, currentTime
-  }) => {
-   io.to(targetId).emit('sync', currentTime);
+  socket.on('host-create', roomId => {
+    socket.join(roomId);
+    socket.isHost = true;
+    rooms[roomId] = rooms[roomId] || { count: 0 };
+    rooms[roomId].count = 1;
+    io.to(roomId).emit('update-count', rooms[roomId].count);
   });
 
- socket.on('control',
-  ({
-   roomId, action, time
-  }) => {
-   // Only host should send this
-   io.to(roomId).emit('control', {
-    action, time
-   });
-  });
-
- socket.on('disconnecting',
-  () => {
-   if (socket.syncInterval) clearInterval(socket.syncInterval);
-   const roomsJoined = Array.from(socket.rooms).filter(r => r !== socket.id);
-   roomsJoined.forEach(roomId => {
-    if (rooms[roomId]) {
-     rooms[roomId].count--;
-     io.to(roomId).emit('update-count', rooms[roomId].count);
-     if (rooms[roomId].count === 0) delete rooms[roomId];
+  socket.on('join-room', roomId => {
+    if (!rooms[roomId]) {
+      socket.emit('error', 'Room does not exist');
+      return;
     }
-   });
+    socket.join(roomId);
+    rooms[roomId].count++;
+    io.to(roomId).emit('update-count', rooms[roomId].count);
+    // Ask host for current time to sync new joiner
+    socket.to(roomId).emit('sync-request', socket.id);
+  });
+
+  socket.on('sync-time', ({ targetId, currentTime }) => {
+    io.to(targetId).emit('sync', currentTime);
+  });
+
+  socket.on('control', ({ roomId, action, time }) => {
+    // Only host should send this
+    io.to(roomId).emit('control', { action, time });
+  });
+
+  socket.on('disconnecting', () => {
+    const roomsJoined = Array.from(socket.rooms).filter(r => r !== socket.id);
+    roomsJoined.forEach(roomId => {
+      if (rooms[roomId]) {
+        rooms[roomId].count--;
+        io.to(roomId).emit('update-count', rooms[roomId].count);
+        if (rooms[roomId].count === 0) delete rooms[roomId];
+      }
+    });
   });
 });
 
